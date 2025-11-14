3b8c25a1bc70ca967452b9bbc543e41643c16b46
Mirile2024 <mirileas@gmail.com>
2025-11-14 00:49:54 -0300

    clase 1 ejercicio1 y 2 completo

--- Clase 1/Ejercicios/actividades/actividad_1.py
+++ Clase 1/Ejercicios/actividades/actividad_1.py
@@ -1,3 +1,5 @@
 # Mensaje de Bienvenida: Crea un programa que use `input()` para pedir al usuario su nombre y luego imprima un mensaje personalizado que lo salude. Usa la función `print()` para concatenar el saludo con el nombre.
 
 # Escribe tu código aquí
+nombre = input("Ingrese su nombre: ")
+print(f"Hola {nombre}, te saludo")
--- Clase 1/Ejercicios/actividades/actividad_2.py
+++ Clase 1/Ejercicios/actividades/actividad_2.py
@@ -1,3 +1,8 @@
 # Información Personal con `input()`: Pide al usuario que ingrese su nombre, edad y ciudad de residencia usando `input()`. Luego, imprime un mensaje que muestre la información ingresada en una sola línea, formateando las variables para que tengan sentido en la oración.
 
 # Escribe tu código aquí
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+ciudad = input("Ingrese su ciudad de residencia: ")
+
+print(f"Hola {nombre}, tu edad es" + str(edad) + f"y vives en {ciudad}")
